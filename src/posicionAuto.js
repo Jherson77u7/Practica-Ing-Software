fecff8b01e612f5a0fdc6e1e5b01d8e121eda5c8
Jherson <Jherson.chambi@ucb.edu.bo>
2024-09-03 20:07:56 +0200

    Estruturando en clases y  dividiendo el trabajo en una mejor estrutura version mejorada

---
+++ src/posicionAuto.js
@@ -0,0 +1,28 @@
+import auto from "./auto";
+
+function verificarFormato(cadena){
+    var result = false;
+    if(typeof cadena == "string"){
+        var formatoValido = /^\d+,\d+[NSOE]$/;
+        if(formatoValido.test(cadena))
+        {
+            result = true;
+        }
+    }
+    return result;
+}
+function posAuto(coordenadas){
+    var pos;
+    if(verificarFormato(coordenadas)){
+        pos = coordenadas;
+    }
+    else{
+        pos = "Formato incorrecto";
+    }
+    return pos;
+}
+
+export default posAuto;
+
+
+
