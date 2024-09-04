fdb5828e81972eb3e25f32ba12cac41ae9329b69
AngelDRS <davidrios1313ma@gmail.com>
2024-09-04 11:20:53 -0300

    Update posAuto.test.js
    Permitir solo un formato de entrada 'X,Y'

--- src/posAuto.test.js
+++ src/posAuto.test.js
@@ -5,5 +5,11 @@ describe("Posicion del auto", () => {
     it("Deberia mostrar en coordenadas x, y la posicion del auto", () => {
       expect(posAuto("1,2")).toEqual("Formato incorrecto");
     });
-
+    it("Solo permite cadenas", () => {
+        expect(posAuto(88)).toEqual("Formato incorrecto");
+    });
+    it("Solo permite el formato establecido: 'x,y' ", () => {
+        expect(posAuto("1,2")).toEqual("Formato incorrecto");
+    });
+    
 });
