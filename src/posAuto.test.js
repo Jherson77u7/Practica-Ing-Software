10c71b560194aa9710df6584e64ef47d9d5e015c
AngelDRS <davidrios1313ma@gmail.com>
2024-09-04 23:46:15 -0300

    Update posAuto.test.js
    posicion N  inicial

--- src/posAuto.test.js
+++ src/posAuto.test.js
@@ -14,4 +14,7 @@ describe("Posicion del auto", () => {
     it("Solo permite el formato establecido: 'x,y' " , () => {
         expect(posAuto("1/2")).toEqual("Formato incorrecto");
     });
+    it("Se añade la orientacion N en la posicion inicial, y solo deja ingresar un formato establecido x,yN" , () => {
+        expect(posAuto("2,5N")).toEqual("2,5N");
+      });
 });
